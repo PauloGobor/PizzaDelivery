f6e5366bc150dfe741ac0843c28fc51628f405ce
Alex Sandro <alexsandroarents@hotmail.com>
2019-05-12 22:25:45 -0300

    Implementado o xml de style do botão

--- app/src/main/java/edu/up/pizzadelivery/view/MainActivity.java
+++ app/src/main/java/edu/up/pizzadelivery/view/MainActivity.java
@@ -1,7 +1,9 @@
-package edu.up.pizzadelivery;
+package edu.up.pizzadelivery.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import edu.up.pizzadelivery.R;
 
 public class MainActivity extends AppCompatActivity {
 
