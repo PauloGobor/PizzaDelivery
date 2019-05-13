316dfac0d60223723960c4bbd601faa3274ba040
Alex Sandro <alexsandroarents@hotmail.com>
2019-05-13 16:34:14 -0300

    Criado as activitys CardapioActivity e CadastroPessoasActivity juntamente com seus arquivos .xml

---
+++ app/src/main/java/edu/up/pizzadelivery/view/CadastroPessoasActivity.java
@@ -0,0 +1,14 @@
+package edu.up.pizzadelivery.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.up.pizzadelivery.R;
+
+public class CadastroPessoasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_pessoas);
+    }
+}
--- app/src/main/java/edu/up/pizzadelivery/view/MainActivity.java
+++ app/src/main/java/edu/up/pizzadelivery/view/MainActivity.java
@@ -1,15 +1,27 @@
 package edu.up.pizzadelivery.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
 import edu.up.pizzadelivery.R;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void TelaCadastroPessoa(View view) {
+        Intent cadastroPessoa =  new Intent(this, CadastroPessoasActivity.class);
+        startActivity(cadastroPessoa);
+    }
+
+    public void TelaCardapio(View view) {
+        Intent cardapio = new Intent(this, CardapioActivity.class);
+        startActivity(cardapio);
     }
 }
