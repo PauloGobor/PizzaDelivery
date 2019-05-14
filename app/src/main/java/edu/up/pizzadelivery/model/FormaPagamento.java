92f8b38e3b0afd19a0fbca2fbd7bfaa4d1a8167a
Paulo Eduardo Gobor <pauloegobor@gmail.com>
2019-05-13 22:18:04 -0300

    add endereco entrega

--- app/src/main/java/edu/up/pizzadelivery/model/FormaPagamento.java
+++ app/src/main/java/edu/up/pizzadelivery/model/FormaPagamento.java
@@ -13,6 +13,11 @@ public class FormaPagamento {
         this.idFormaPagamento = idFormaPagamento;
     }
 
+    public String getNome() {
+        return Nome;
+    }
 
-
+    public void setNome(String nome) {
+        Nome = nome;
+    }
 }
--- app/src/main/java/edu/up/pizzadelivery/model/Pedido.java
+++ app/src/main/java/edu/up/pizzadelivery/model/Pedido.java
@@ -9,6 +9,7 @@ public class Pedido {
     private Usuario usuario;
     private List<ItemPedido> itensPedido;
     private FormaPagamento formaPagamento;
+    private Endereco endEntrega;
     private Date Data;
 
     public int getIdPedido() {
@@ -33,6 +34,22 @@ public class Pedido {
 
     public void setItensPedido(List<ItemPedido> itensPedido) {
         this.itensPedido = itensPedido;
+    }
+
+    public FormaPagamento getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(FormaPagamento formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public Endereco getEndEntrega() {
+        return endEntrega;
+    }
+
+    public void setEndEntrega(Endereco endEntrega) {
+        this.endEntrega = endEntrega;
     }
 
     public Date getData() {
