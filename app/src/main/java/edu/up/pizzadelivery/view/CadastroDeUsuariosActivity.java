1ef8a1ed7abff960f6595251c08ea2a8dcee8aea
Alex Sandro <alexsandroarents@hotmail.com>
2019-05-15 14:32:09 -0300

    Realizado a modificacao do nome da classe activity CadastroPessoa para CadastroDeUsuarios e adcionado os componentes na MainActivity

--- app/src/main/java/edu/up/pizzadelivery/view/CadastroDeUsuariosActivity.java
+++ app/src/main/java/edu/up/pizzadelivery/view/CadastroDeUsuariosActivity.java
@@ -1,17 +1,16 @@
 package edu.up.pizzadelivery.view;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import edu.up.pizzadelivery.R;
 
-public class CadastroPessoasActivity extends AppCompatActivity {
+public class CadastroDeUsuariosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro_pessoas);
+        setContentView(R.layout.activity_cadastro_de_usuarios);
 
         // comandos a seguir estao temporarios
         // ate que possa ser separado em camadas...
--- app/src/main/java/edu/up/pizzadelivery/view/MainActivity.java
+++ app/src/main/java/edu/up/pizzadelivery/view/MainActivity.java
@@ -4,22 +4,49 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import edu.up.pizzadelivery.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText edtEmail, edtSenha;
+    private Button btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtEmail  = (EditText) findViewById(R.id.edtEmail);
+        edtSenha  = (EditText) findViewById(R.id.edtSenha);
+        btnEntrar = (Button)   findViewById(R.id.btnEntrar);
+
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!edtEmail.getText().toString().equals("") && !edtSenha.getText().toString().equals("")) {
+
+
+
+                }else{
+                    Toast.makeText(MainActivity.this, "Favor preencher os campos de login!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
+
+    ///Comando que chama a tela de cadastro
     public void TelaCadastroPessoa(View view) {
-        Intent cadastroPessoa =  new Intent(this, CadastroPessoasActivity.class);
+        Intent cadastroPessoa = new Intent(this, CadastroDeUsuariosActivity.class);
         startActivity(cadastroPessoa);
     }
 
+    ///Comando que chama a tela de cardápio
     public void TelaCardapio(View view) {
         Intent cardapio = new Intent(this, CardapioActivity.class);
         startActivity(cardapio);
