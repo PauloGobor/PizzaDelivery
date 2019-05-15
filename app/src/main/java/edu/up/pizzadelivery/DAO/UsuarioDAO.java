62a233f0e9fa52b8587fc74a1032b5a40cb0979e
Alex Sandro <alexsandroarents@hotmail.com>
2019-05-15 18:29:13 -0300

    Arrumado o metodo ValidaLogin

--- app/src/main/java/edu/up/pizzadelivery/DAO/UsuarioDAO.java
+++ app/src/main/java/edu/up/pizzadelivery/DAO/UsuarioDAO.java
@@ -7,19 +7,15 @@ import edu.up.pizzadelivery.model.Usuario;
 
 public class UsuarioDAO {
 
-<<<<<<< HEAD
+
     private static boolean valorReferencia;
-=======
-    public static long cadastrarCidade(Context context, Usuario usuario){
-        BancoDados banco = new BancoDados(context);
 
-        return banco.CadastrarUsuario(usuario);
-
-    }
-
-
-    /*public static long ValidarLogin(Context context, Login login){
->>>>>>> 29c422762773c46bba3515249fd1b42e7f4b1e72
+//    public static long cadastrarCidade(Context context, Usuario usuario){
+//        BancoDados banco = new BancoDados(context);
+//
+//        return banco.CadastrarUsuario(usuario);
+//
+//    }
 
     public static boolean ValidarLogin(Context context, Login login){
 
