5573b78620e04b152107e7bc06b2f5b732fa4e8f
Paulo Eduardo Gobor <pauloegobor@gmail.com>
2019-05-25 14:48:14 -0300

    inserdo EscolherTamanhoActivity

---
+++ app/src/main/java/edu/up/pizzadelivery/view/EscolherTamanhoActivity.java
@@ -0,0 +1,15 @@
+package edu.up.pizzadelivery.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.up.pizzadelivery.R;
+
+public class EscolherTamanhoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escolher_tamanho);
+    }
+}
