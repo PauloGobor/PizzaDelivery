ec75043b24c18fa1f39a253220ca80b895f1c7d7
Paulo Eduardo Gobor <pauloegobor@gmail.com>
2019-06-07 14:18:04 -0300

    carrinhoActivity

---
+++ app/src/main/java/edu/up/pizzadelivery/view/CarrinhoActivity.java
@@ -0,0 +1,18 @@
+package edu.up.pizzadelivery.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.up.pizzadelivery.R;
+
+public class CarrinhoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carrinho);
+
+
+
+    }
+}
