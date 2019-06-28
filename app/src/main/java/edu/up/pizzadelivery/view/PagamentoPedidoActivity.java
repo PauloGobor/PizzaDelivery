64b56f398d833a40eca52c0af9dbdb07b1ff8180
Alex Sandro <alexsandroarents@hotmail.com>
2019-06-28 00:07:11 -0300

    Implementado Troco

--- app/src/main/java/edu/up/pizzadelivery/view/PagamentoPedidoActivity.java
+++ app/src/main/java/edu/up/pizzadelivery/view/PagamentoPedidoActivity.java
@@ -1,11 +1,16 @@
 package edu.up.pizzadelivery.view;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,6 +29,8 @@ public class PagamentoPedidoActivity extends AppCompatActivity {
 
     private ListView ListFormasPagameto;
     private TextView PagarTotalPedido;
+    private EditText edtTroco;
+    private Button btnFinalizarTroco;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +42,8 @@ public class PagamentoPedidoActivity extends AppCompatActivity {
 
         ListFormasPagameto = (ListView) findViewById(R.id.ListFormaPagamento);
         PagarTotalPedido = (TextView) findViewById(R.id.PagarTotalPedido);
+        edtTroco = findViewById(R.id.edtTroco);
+        btnFinalizarTroco = findViewById(R.id.btnFinalizarTroco);
 
         PagarTotalPedido.setText("R$: "+String.valueOf(subtotal));
 
@@ -57,9 +66,37 @@ public class PagamentoPedidoActivity extends AppCompatActivity {
 
         ListFormasPagameto.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
 
-                if(formaPagamentosArrayList.get(position).equals(3)){
+                int posicaoClic =  formaPagamentosArrayList.get(position).getId();
+                if(posicaoClic == 3){
+
+                    final int posicaoPag = formaPagamentosArrayList.get(position).getId();
+                    final String posicaoNome = formaPagamentosArrayList.get(position).getNome();
+                    edtTroco.setEnabled(true);
+                    btnFinalizarTroco.setEnabled(true);
+
+                    btnFinalizarTroco.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            Pedido p = new Pedido();
+                            FormaPagamento f = new FormaPagamento();
+                            f.setId(posicaoPag);
+                            f.setNome(posicaoNome);
+                            p.setFormaPagamento(f);
+                            p.setTotal(subtotal);
+
+                            long updatePagamento  =  FormaPagamentoDAO.AtualizaFormaPagamento(PagamentoPedidoActivity.this, p, idPedido);
+
+                            if(updatePagamento != -1){
+                                MenssagemConfCad();
+                            }else {
+                                Toast.makeText(PagamentoPedidoActivity.this, "Erro Ao Finalizar Pedido!", Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    });
 
                 }else {
 
@@ -73,8 +110,7 @@ public class PagamentoPedidoActivity extends AppCompatActivity {
                     long updatePagamento  =  FormaPagamentoDAO.AtualizaFormaPagamento(PagamentoPedidoActivity.this, p, idPedido);
 
                     if(updatePagamento != -1){
-                        Toast.makeText(PagamentoPedidoActivity.this, "Pedido Finalizado Com Sucesso", Toast.LENGTH_LONG).show();
-
+                        MenssagemConfCad();
 
                     }else {
                         Toast.makeText(PagamentoPedidoActivity.this, "Erro Ao Finalizar Pedido!", Toast.LENGTH_LONG).show();
@@ -85,9 +121,25 @@ public class PagamentoPedidoActivity extends AppCompatActivity {
 
             }
         });
+    }
+    private void MenssagemConfCad() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pedido realizado com sucesso ");
+        builder.setMessage("Você será redireiconado para a sua area onde poderá verifcar o historico de pedido!");
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent telaLogin = new Intent(PagamentoPedidoActivity.this, AreaClienteActivity.class);
+                startActivity(telaLogin);
+            }
+        });
 
 
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
 
     }
+
 
 }
